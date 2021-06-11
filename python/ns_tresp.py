83e7e62de09719004fecf393fe2e41746255b6f0
Iain Hannah <iain.hannah@glasgow.ac.uk>
2021-06-11 18:41:31 +0100

    Updated to Kris' faster version of rmf load

--- python/ns_tresp.py
+++ python/ns_tresp.py
@@ -155,7 +155,7 @@ def col2arr_py(data, **kwargs):
     return chan_array_py
 
 
-def vrmf2arr_py(data=None, n_grp_list=None, f_chan_array=None, n_chan_array=None):
+def vrmf2arr_py(data=None, n_grp_list=None, f_chan_array=None, n_chan_array=None, **kwargs):
     ''' Takes redistribution parameters for each energy channel from a .rmf file and returns it in the correct format.
     From: https://lost-contact.mit.edu/afs/physics.wisc.edu/home/craigm/lib/idl/spectral/vrmf2arr.pro
     
@@ -165,8 +165,9 @@ def vrmf2arr_py(data=None, n_grp_list=None, f_chan_array=None, n_chan_array=None
             Redistribution matrix parameter array/list from the .rmf file. Units are counts per photon.
             Default : None
             
-    n_grp_list : list
-            Number of groups (sub-channels) for each energy bin. Total number of channels is the length of this list.
+    no_of_channels : int
+            Number of entries is the total number of photon channels, the entries themselves show the total number 
+            of count channels to which that photon channel contributes.
             Default : None
             
     f_chan_array : numpy.array
@@ -176,21 +177,27 @@ def vrmf2arr_py(data=None, n_grp_list=None, f_chan_array=None, n_chan_array=None
     n_chan_array : numpy.array
             The number of sub-set channels in each index for each energy bin from the .rmf file run through col2arr_py().
             Default : None
+    kwargs : idl_check=Bool or idl_way=Bool
+            If idl_check=True the funciton will throw an error if the Python and IDL methods give different answers (they shouldn't).
+            If idl_way=True the IDL method's result with be returned instead of the new Python method described.
             
     Returns
     -------
     A 2D numpy array of the correctly ordered input data with dimensions of energy in the rows and channels in 
     the columns.
     
-    Example
+    Code Example
     -------
-    data = FITS_rec([(  1.6 ,   1.64,   1, [0]   , [18]  , [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]), 
-                     (  1.64,   1.68,   1, [0]   , [20]  , [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
-                     (  1.68,   1.72,   2, [0,22], [20,1], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]), ...,
-                     dtype=(numpy.record, [('ENERG_LO', '>f4'), ('ENERG_HI', '>f4'), ('N_GRP', '>i2'), 
-                                           ('F_CHAN', '>i4', (2,)), ('N_CHAN', '>i4', (2,)), ('MATRIX', '>i4', (2,))]))
-                          
-    >>> vrmf2arr_py(data['MATRIX'])
+    >>> d_rmf = 'directory/'
+    >>> f_rmf = 'file.rmf'
+    >>> e_lo, e_hi, ngrp, fchan, nchan, matrix = nu_spec.read_rmf(d_rmf+f_rmf)
+    >>> fchan_array = nu_spec.col2arr_py(fchan)
+    >>> nchan_array = nu_spec.col2arr_py(nchan)
+    >>> rmf = nu_spec.vrmf2arr_py(data=matrix,  
+                                  n_grp_list=ngrp,
+                                  f_chan_array=fchan_array, 
+                                  n_chan_array=nchan_array)
+    >>> rmf
     array([[0.00033627, 0.0007369 , 0.00113175, ..., 0.        , 0.        , 0.        ],
            [0.00039195, 0.00079259, 0.00138341, ..., 0.        , 0.        , 0.        ],
            [0.00042811, 0.00083381, 0.00157794, ..., 0.        , 0.        , 0.        ],
@@ -198,54 +205,131 @@ def vrmf2arr_py(data=None, n_grp_list=None, f_chan_array=None, n_chan_array=None
            [0.        , 0.        , 0.        , ..., 0.00408081, 0.00409889, 0.00403308],
            [0.        , 0.        , 0.        , ..., 0.00405333, 0.00413722, 0.00413216],
            [0.        , 0.        , 0.        , ..., 0.        , 0.        , 0.        ]])
-    ## rows = energy, columns = channels 
+    ## rows = photon/energy channels, columns = counts channels 
+    What's Going On?
+    ----------------
+    The RMF file has the photon-to-counts conversion information in it. 
+    The martix has the photon-to-count conversion value for each count channel (columns) that is involved with theach photon channel (rows). 
+            E.g., matrix = [ [a, b, c, d, e, f, ...] , 
+                             [        ...          ] , 
+                             [        ...          ] , 
+                                      ...             ]
+    F_chan is the starting index of contiguous counts channels that are involved with the photon channel. 
+            E.g., f_chan = [ [0, 5, 0, 0, 0, ...] , 
+                             [       ...        ] , 
+                             [       ...        ] , 
+                                     ...           ] 
+                            For the first photon channel, there are rows of counts channels starting at index 0 and 5
+    N_chan is the corresponding number of counts channels from each index in the f_chan array.
+            E.g., n_chan = [ [2, 3, 0, 0, 0, ...] , 
+                             [        ...        ] , 
+                             [        ...        ] , 
+                                      ...           ]
+                            Starting at index 0 for the first photon channel we have the first 2 matrix values, then at index 5 we have the next 3.
+                            The total of each row is the same as the n_grp_list and the number of entries in each row of the matrix entry.
+    Putting all this together, the rmf matrix is:
+            rmf_matrix = [ [a, b, 0, 0, 0, c , d , e, 0 , 0 , ...] ,   #<-- index 0 (f_chan) with 2 entries (n_chan) with photon-to-counts conversion (matrix)
+                         [                 ...                   ] , 
+                         [                 ...                   ] , 
+                                           ...                      ] 
     '''
     
-    # unravel matrix array, can't use numpy.ravel as this has variable length rows
-    ## now can index the start of each row with the running total
-    unravel_dmat = []
-    for n in data:
-        for nn in n:
-            unravel_dmat.append(nn)
+    # this was is about >6x quicker in than the IDL code written in Python
+    
+    # find the non-zero entries in Nchan, this is the number to counts channels 
+    #  in a row that contribute so will have a value if it is useful
+    b = np.nonzero(n_chan_array)
+    
+    # now only want the useful entries from the pre-formatted Nchan and Fchan arrays
+    c = f_chan_array[b]
+    d = n_chan_array[b]
+    
+    # to help with indexing, this provides a running sum of the number of counts 
+    #  channels that a single photon channel contributes to
+    e = np.cumsum(n_chan_array, axis=1)
 
-    no_of_channels = len(n_grp_list)
+    # these entries will give the final indices in the row on counts channels
+    final_inds = e[b]
 
-    nrows = len(data)
-    ncols = no_of_channels
-    nc = np.array([len(n) for n in data])
-    accum_nc_almost = [nc[i]+sum(nc[0:i]) for i in range(len(nc))]
-    accum_nc = np.array([0] + accum_nc_almost) 
-    # sorted wobble of diagonal lines, the indices were off by one left and right
-    ## i.e. this is the running index so should start at zero
+    # need to find the starting index so -1, but that means any entry that is 
+    #  -1 will be where a zero is needed
+    starting_inds = b[1]-1
 
-    mat_array = np.zeros(shape=(nrows, ncols))
+    # get the  starting indices but the ones that should be 0 are replaced with 
+    #  the final on in the list at the minute (-1 in starting_inds)
+    start_inds = np.cumsum(n_chan_array, axis=1)[(b[0], starting_inds)] 
 
-    for r in range(nrows):
-        if nc[r] > 0:
-            # in IDL code the second index is -1 but that's because IDL's index boundaries 
-            ## are both inclusive so drop the -1, i.e. was accum_nc[r+1]-1
-            row = unravel_dmat[accum_nc[r]:accum_nc[r+1]] 
+    # where starting_inds==-1 that value should be 0, i.e. starting from the first 
+    #  value in the rmf matrix
+    new_e = np.where(starting_inds!=-1, start_inds, 0)
 
-            c=0
+    # initialise the rmf matrix
+    mat_array_py = np.zeros((len(data),len(n_grp_list)))
+    
+    # now go through row by row (this is the slowest part and needs to be made faster).
+    #  Here we go through each photon channel's number of discrete rows of counts channels.
+    for r in range(len(c)):
+        mat_array_py[b[0][r], c[r]:c[r]+d[r]] = data[b[0][r]][new_e[r]:final_inds[r]]
 
-            # for number of sub-set channels in each energy channel groups
-            for ng in range(n_grp_list[r]):
-                # want redist. prob. for number of sub-set channels 
-                ## if c+m is larger than len(row)-1 then only want what we can get
-                wanted_r = [row[int(c+m)] for m in np.arange(n_chan_array[r,ng]) if c+m <= len(row)-1 ]
 
-                # now fill in the entries in mat_array from the starting number of the sub-set channel, 
-                ## the fchan_array[r, ng]
-                for z,wr in enumerate(wanted_r):
-                    mat_array[r, int(f_chan_array[r, ng])+z] = wr
+    #*************************************************************************************************************************************************
+    # if you want to involve the IDL way
+    # set defaults to help check how it is done in IDL (second dict rewrites the keys of the first)
+    defaults = {**{"idl_check":False, "idl_way":False}, **kwargs}
 
-                # move the place that the that the index for row starts from along 
-                c = c + n_chan_array[r,ng]
+    if defaults["idl_check"] or defaults["idl_way"]:
+        # unravel matrix array, can't use numpy.ravel as this has variable length rows
+        ## now can index the start of each row with the running total
+        unravel_dmat = []
+        for n in data:
+            for nn in n:
+                unravel_dmat.append(nn)
 
-            # if dgrp[r] == 0 then above won't do anything, need this as not to miss out the 0th energy channel
-            if n_grp_list[r] == 0:
-                wanted_r = [row[int(c+m)] for m in np.arange(n_chan_array[r,0]) if c+m <= len(row)-1 ]
-                for z,wr in enumerate(wanted_r):
-                    mat_array[r, int(f_chan_array[r, 0])+z] = wr
+        no_of_channels = len(n_grp_list)
+
+        nrows = len(data)
+        ncols = no_of_channels
+        nc = np.array([len(n) for n in data])
+        accum_nc_almost = [nc[i]+sum(nc[0:i]) for i in range(len(nc))]
+        accum_nc = np.array([0] + accum_nc_almost) 
+        # sorted wobble of diagonal lines, the indices were off by one left and right
+        ## i.e. this is the running index so should start at zero
+
+        mat_array = np.zeros(shape=(nrows, ncols))
+
+        for r in range(nrows):
+            if nc[r] > 0:
+                # in IDL code the second index is -1 but that's because IDL's index boundaries 
+                ## are both inclusive sod rop the -1, i.e. was accum_nc[r+1]-1
+                row = unravel_dmat[accum_nc[r]:accum_nc[r+1]] 
+
+                c=0
+
+                # for number of sub-set channels in each energy channel groups
+                for ng in range(n_grp_list[r]):
+                    # want redist. prob. for number of sub-set channels 
+                    ## if c+m is larger than len(row)-1 then only want what we can get
+                    wanted_r = [row[int(c+m)] for m in np.arange(n_chan_array[r,ng]) if c+m <= len(row)-1 ]
+
+                    # now fill in the entries in mat_array from the starting number of the sub-set channel, 
+                    ## the fchan_array[r, ng]
+                    for z,wr in enumerate(wanted_r):
+                        mat_array[r, int(f_chan_array[r, ng])+z] = wr
+
+                    # move the place that the that the index for row starts from along 
+                    c = c + n_chan_array[r,ng]
+
+                # if dgrp[r] == 0 then above won't do anything, need this as not to miss out the 0th energy channel
+                if n_grp_list[r] == 0:
+                    wanted_r = [row[int(c+m)] for m in np.arange(n_chan_array[r,0]) if c+m <= len(row)-1 ]
+                    for z,wr in enumerate(wanted_r):
+                        mat_array[r, int(f_chan_array[r, 0])+z] = wr
+
+        if defaults["idl_check"]:
+            assert np.array_equal(mat_array_py, mat_array), \
+            "The IDL way and the Python way here do not produce the same result. \nPlease check this but trust the IDL way more (set idl_way=True)!"
+        if defaults["idl_way"]:
+            return mat_array
+    #*************************************************************************************************************************************************
                     
-    return mat_array
+    return mat_array_py
